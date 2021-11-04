d1ae6d1a23a366a558fbd8dbf1288ca979c710bc
aleksey899 <sances492@gmail.com>
2021-11-04 12:24:15 +0300

    Entity

---
+++ Controllers/AdministratorEnityController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Employment.Controllers
+{
+    [ApiController]
+    [Route("/AdministratorEntity")]
+    public class AdministratorEntityController : ControllerBase
+    {
+
+        [HttpPut("Create")]
+        public string Create(string str)
+        {
+            return str;
+        }
+
+        [HttpGet("Read")]
+        public string Read(string str)
+        {
+            return str;
+        }
+
+        [HttpPatch("Update")]
+        public string Update(string str)
+        {
+            return str;
+        }
+
+        [HttpDelete("Delete")]
+        public string Delete(string str)
+        {
+            return str;
+        }
+
+
+    }
+}
